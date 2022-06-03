e658a6c0f9f8a4f831a74b1e3915e279bbb8f387
Sonny2203 <106823776+Sonny2203@users.noreply.github.com>
2022-06-03 21:42:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "ĐÔI LỜI CỦA NGUYỄN PHÚC HƯNG <3",
+    introTitle: 'VÕ THỊ XUÂN NHƯ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messDesc: 'TỐI NAY NHỚ NGỦ SỚM NHA!!!',
+    btnAccept: 'Ố Kề lun <3 yêu anh',
+    messLink: 'https://www.youtube.com/watch?v=wdQoVMlVMGY' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
